0cf94acbaebbea4dd3a71b286aa3b22ca72775a1
Iris <iris.perttuli@gmail.com>
2023-11-29 22:43:31 +0200

    Fix pylint

--- src/stub_io.py
+++ src/stub_io.py
@@ -1,5 +1,6 @@
 import traceback
 
+
 class StubIO:
     def __init__(self, inputs=None):
         self.inputs = inputs or []
@@ -8,19 +9,19 @@ class StubIO:
     def write(self, value):
         self.outputs.append(value)
 
-    # def read(self, _):
-    #     if len(self.inputs) > 0:
-    #         try:
-    #             return self.inputs.pop(0)
-    #         except IndexError:
-    #             print(traceback.format_exc())
-    #     return ""
     def read(self, _):
-        if self.inputs:
-            return self.inputs.pop(0)
-        else: 
-            print("Field cannot be empty. Please provide a valid input.")
+        if len(self.inputs) > 0:
+            try:
+                return self.inputs.pop(0)
+            except IndexError:
+                print(traceback.format_exc())
         return ""
+    # def read(self, _):
+    #     if self.inputs:
+    #         return self.inputs.pop(0)
+    #     else:
+    #         print("Field cannot be empty. Please provide a valid input.")
+    #     return ""
 
     def add_input(self, value):
         self.inputs.append(value)

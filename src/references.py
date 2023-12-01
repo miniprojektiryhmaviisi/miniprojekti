a3bde5bf2f9081f67ffac24bd92ff17c4d87931e
Iris <iris.perttuli@gmail.com>
2023-12-01 14:29:56 +0200

    Create bib-file that works

--- src/references.py
+++ src/references.py
@@ -19,6 +19,7 @@ class References:
         # sleep(1)
         io_handler.write("Type 2 to Exit")
         # sleep(1)
+        io_handler.write("Type 'b' to make your existing references into bibtex form")
         command = io_handler.read("What do you want to do? ")
         if command == "0":
             self.add(io_handler, service)
@@ -99,8 +100,79 @@ class References:
             io_handler.write("Exiting...")
             sleep(1)
             return
+        elif command=="b":
+            with open("file.bib", "w") as file:
+                all_book_refs=self.service.get_all_books()
+                for i in all_book_refs:
+                    file.write("@book{" + i[0] + ",\n")
+                    file.write("   author = {" + i[1] + "},\n")
+                    file.write("   title = {" + i[2] + "},\n")
+                    file.write("   year = {" + str(i[4]) + "},\n")
+                    file.write("   publisher = {" + i[3] + "},\n")
+                    if i[5] != "":
+                        file.write("   volume = {" + str(i[5]) + "},\n")
+                    if i[6] != "":
+                        file.write("   number = {" + str(i[6]) + "},\n")
+                    if i[7] != "":
+                        file.write("   pages = {" + i[7] + "},\n")
+                    if i[8] != "":
+                        file.write("   month = {" + str(i[8]) +"- },\n")
+                    if i[9] != "":
+                        file.write("   note = {" + i[9] + "},\n")
+                    file.write("}\n\n")
+
+                all_article_refs=self.service.get_all_articles()
+                for j in all_article_refs:
+                    file.write("@article{" + j[0] + ",\n")
+                    file.write("   author = {" + j[1] + "},\n")
+                    file.write("   title = {{" + j[2] + "}},\n") 
+                    file.write("   year = {" + str(j[4]) + "},\n")
+                    file.write("   journal = {" + j[3] + "},\n")
+                    if j[5] != "":
+                        file.write("   volume = {" + str(j[5]) + "},\n")
+                    if j[6] != "":
+                        file.write("   number = {" + str(j[6]) + "},\n")
+                    if j[7] != "":
+                        file.write("   pages = {" + j[7] + "},\n")
+                    if j[8] != "":
+                        file.write("   month = {" + str(j[8]) + "},\n")
+                    if j[9] != "":
+                        file.write("   note = {" + j[9] + "},\n")
+                    file.write("}\n\n")
+
+                all_inproceeding_refs=self.service.get_all_inproceedings()
+                for j in all_inproceeding_refs:
+                    file.write("@inproceedings{" + j[0] + ",\n")
+                    file.write("   author = {" + j[1] + "},\n")
+                    file.write("   title = {{" + j[2] + "}},\n")
+                    file.write("   booktitle = {" + j[3] + "},\n")
+                    file.write("   year = {" + str(j[4]) + "},\n")
+                    if j[5] != "":
+                        file.write("   editor = {" + j[5] + "},\n")
+                    if j[6] != "":
+                        file.write("   volume = {" + str(j[6]) + "},\n")
+                    if j[7] != "":
+                        file.write("   number = {" + str(j[7]) + "},\n")
+                    if j[8] != "":
+                        file.write("   series = {" + str(j[8]) + "},\n")
+                    if j[9] != "":
+                        file.write("   pages = {" + j[9] + "},\n")
+                    if j[10] != "":
+                        file.write("   address = {" + j[10] + "},\n")
+                    if j[11] != "":
+                        file.write("   month = {" + str(j[11]) +"- },\n")
+                    if j[12] != "":
+                        file.write("   organization = {" + j[12] + "},\n")
+                    if j[13] != "":
+                        file.write("   publisher = {" + j[13] + "},\n")
+                    if j[14] != "":
+                        file.write("   note = {" + j[14] + "},\n")
+                    file.write("}\n\n")
+
+                    
+
         else:
-            io_handler.write("Invalid input. Please enter '0', '1' or '2'.")
+            io_handler.write("Invalid input. Please enter '0', '1', '2' or 'b'.")
             sleep(2)
             self.welcome(io_handler, service)
 

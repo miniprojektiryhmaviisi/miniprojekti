0ebd36392ebb55d3e876f06ecb976035fbc5e1bd
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2023-12-04 14:51:06 +0200

    updated references structure

--- src/references.py
+++ src/references.py
@@ -17,7 +17,9 @@ class References:
         # sleep(1)
         io_handler.write("Type 1 for View my references")
         # sleep(1)
-        io_handler.write("Type 2 to Exit")
+        io_handler.write("Type 2 to Search")
+        # sleep(1)
+        io_handler.write("Type 3 to Exit")
         # sleep(1)
         io_handler.write("Type 'b' to make your existing references into bibtex form")
         command = io_handler.read("What do you want to do? ")
@@ -25,152 +27,17 @@ class References:
             self.add(io_handler, service)
             self.welcome(io_handler, service)
         elif command == "1":
-            io_handler.write("")
-            io_handler.write("---------------")
-            io_handler.write("Book references")
-            io_handler.write("---------------\n ")
-            for entry in self.service.get_all_books():
-                io_handler.write("Cite Key     : " + entry[0])
-                io_handler.write("Author       : " + entry[1])
-                io_handler.write("Title        : " + entry[2])
-                io_handler.write("Publisher    : " + entry[3])
-                io_handler.write("Year         : " + str(entry[4]))
-                if entry[5] != "":
-                    io_handler.write("Volume       : " + str(entry[5]))
-                if entry[6] != "":
-                    io_handler.write("Number       : " + str(entry[6]))
-                if entry[7] != "":
-                    io_handler.write("Pages        : " + entry[7])
-                if entry[8] != "":
-                    io_handler.write("Month        : " + str(entry[8]))
-                if entry[9] != "":
-                    io_handler.write("Notes        : " + entry[9])
-                io_handler.write("---------------")
-            io_handler.write("Article references")
-            io_handler.write("---------------\n ")
-            for entry in self.service.get_all_articles():
-                io_handler.write("Cite Key     : " + entry[0])
-                io_handler.write("Author       : " + entry[1])
-                io_handler.write("Title        : " + entry[2])
-                io_handler.write("Journal      : " + entry[3])
-                io_handler.write("Year         : " + str(entry[4]))
-                if entry[5] != "":
-                    io_handler.write("Volume       : " + str(entry[5]))
-                if entry[6] != "":
-                    io_handler.write("Number       : " + str(entry[6]))
-                if entry[7] != "":
-                    io_handler.write("Pages        : " + entry[7])
-                if entry[8] != "":
-                    io_handler.write("Month        : " + str(entry[8]))
-                if entry[9] != "":
-                    io_handler.write("Notes        : " + entry[9])
-                io_handler.write("---------------")
-            io_handler.write("Inproceedings references")
-            io_handler.write("---------------\n ")
-            for entry in self.service.get_all_inproceedings():
-                io_handler.write("Cite Key     : " + entry[0])
-                io_handler.write("Author       : " + entry[1])
-                io_handler.write("Title        : " + entry[2])
-                io_handler.write("Book Title   : " + entry[3])
-                io_handler.write("Year         : " + str(entry[4]))
-                if entry[5] != "":
-                    io_handler.write("Editor       : " + entry[5])
-                if entry[6] != "":
-                    io_handler.write("Volume       : " + str(entry[6]))
-                if entry[7] != "":
-                    io_handler.write("Number       : " + str(entry[7]))
-                if entry[8] != "":
-                    io_handler.write("Series       : " + str(entry[8]))
-                if entry[9] != "":
-                    io_handler.write("Pages        : " + entry[9])
-                if entry[10] != "":
-                    io_handler.write("Address      : " + entry[10])
-                if entry[11] != "":
-                    io_handler.write("Month        : " + str(entry[11]))
-                if entry[12] != "":
-                    io_handler.write("Organization : " + entry[12])
-                if entry[13] != "":
-                    io_handler.write("Publisher    : " + entry[13])
-                if entry[14] != "":
-                    io_handler.write("Note         : " + entry[14])
-                io_handler.write("---------------")
-            sleep(2)
-            self.welcome(io_handler, service)
+            self.view_references(io_handler, service)
         elif command == "2":
+            io_handler.write("Not yet implemented")
+            sleep(1)
+            self.welcome(io_handler, service)
+        elif command == "3":
             io_handler.write("Exiting...")
             sleep(1)
             return
         elif command=="b":
-            with open("file.bib", "w") as file:
-                all_book_refs=self.service.get_all_books()
-                for i in all_book_refs:
-                    file.write("@book{" + i[0] + ",\n")
-                    file.write("   author = {" + i[1] + "},\n")
-                    file.write("   title = {" + i[2] + "},\n")
-                    file.write("   year = {" + str(i[4]) + "},\n")
-                    file.write("   publisher = {" + i[3] + "},\n")
-                    if i[5] != "":
-                        file.write("   volume = {" + str(i[5]) + "},\n")
-                    if i[6] != "":
-                        file.write("   number = {" + str(i[6]) + "},\n")
-                    if i[7] != "":
-                        file.write("   pages = {" + i[7] + "},\n")
-                    if i[8] != "":
-                        file.write("   month = {" + str(i[8]) +"- },\n")
-                    if i[9] != "":
-                        file.write("   note = {" + i[9] + "},\n")
-                    file.write("}\n\n")
-
-                all_article_refs=self.service.get_all_articles()
-                for j in all_article_refs:
-                    file.write("@article{" + j[0] + ",\n")
-                    file.write("   author = {" + j[1] + "},\n")
-                    file.write("   title = {{" + j[2] + "}},\n") 
-                    file.write("   year = {" + str(j[4]) + "},\n")
-                    file.write("   journal = {" + j[3] + "},\n")
-                    if j[5] != "":
-                        file.write("   volume = {" + str(j[5]) + "},\n")
-                    if j[6] != "":
-                        file.write("   number = {" + str(j[6]) + "},\n")
-                    if j[7] != "":
-                        file.write("   pages = {" + j[7] + "},\n")
-                    if j[8] != "":
-                        file.write("   month = {" + str(j[8]) + "},\n")
-                    if j[9] != "":
-                        file.write("   note = {" + j[9] + "},\n")
-                    file.write("}\n\n")
-
-                all_inproceeding_refs=self.service.get_all_inproceedings()
-                for j in all_inproceeding_refs:
-                    file.write("@inproceedings{" + j[0] + ",\n")
-                    file.write("   author = {" + j[1] + "},\n")
-                    file.write("   title = {{" + j[2] + "}},\n")
-                    file.write("   booktitle = {" + j[3] + "},\n")
-                    file.write("   year = {" + str(j[4]) + "},\n")
-                    if j[5] != "":
-                        file.write("   editor = {" + j[5] + "},\n")
-                    if j[6] != "":
-                        file.write("   volume = {" + str(j[6]) + "},\n")
-                    if j[7] != "":
-                        file.write("   number = {" + str(j[7]) + "},\n")
-                    if j[8] != "":
-                        file.write("   series = {" + str(j[8]) + "},\n")
-                    if j[9] != "":
-                        file.write("   pages = {" + j[9] + "},\n")
-                    if j[10] != "":
-                        file.write("   address = {" + j[10] + "},\n")
-                    if j[11] != "":
-                        file.write("   month = {" + str(j[11]) +"- },\n")
-                    if j[12] != "":
-                        file.write("   organization = {" + j[12] + "},\n")
-                    if j[13] != "":
-                        file.write("   publisher = {" + j[13] + "},\n")
-                    if j[14] != "":
-                        file.write("   note = {" + j[14] + "},\n")
-                    file.write("}\n\n")
-
-                    
-
+            self.export_bibtex_file(io_handler, service)
         else:
             io_handler.write("Invalid input. Please enter '0', '1', '2' or 'b'.")
             sleep(2)
@@ -299,3 +166,147 @@ class References:
             raise NotImplementedError
         self.io_handler.write("New reference added!")
         sleep(2)
+
+    def view_references(self, io_handler, service):
+        io_handler.write("")
+        io_handler.write("---------------")
+        io_handler.write("Book references")
+        io_handler.write("---------------\n ")
+        for entry in self.service.get_all_books():
+            io_handler.write("Cite Key     : " + entry[0])
+            io_handler.write("Author       : " + entry[1])
+            io_handler.write("Title        : " + entry[2])
+            io_handler.write("Publisher    : " + entry[3])
+            io_handler.write("Year         : " + str(entry[4]))
+            if entry[5] != "":
+                io_handler.write("Volume       : " + str(entry[5]))
+            if entry[6] != "":
+                io_handler.write("Number       : " + str(entry[6]))
+            if entry[7] != "":
+                io_handler.write("Pages        : " + entry[7])
+            if entry[8] != "":
+                io_handler.write("Month        : " + str(entry[8]))
+            if entry[9] != "":
+                io_handler.write("Notes        : " + entry[9])
+        io_handler.write("---------------")
+        io_handler.write("Article references")
+        io_handler.write("---------------\n ")
+        for entry in self.service.get_all_articles():
+            io_handler.write("Cite Key     : " + entry[0])
+            io_handler.write("Author       : " + entry[1])
+            io_handler.write("Title        : " + entry[2])
+            io_handler.write("Journal      : " + entry[3])
+            io_handler.write("Year         : " + str(entry[4]))
+            if entry[5] != "":
+                io_handler.write("Volume       : " + str(entry[5]))
+            if entry[6] != "":
+                io_handler.write("Number       : " + str(entry[6]))
+            if entry[7] != "":
+                io_handler.write("Pages        : " + entry[7])
+            if entry[8] != "":
+                io_handler.write("Month        : " + str(entry[8]))
+            if entry[9] != "":
+                io_handler.write("Notes        : " + entry[9])
+            io_handler.write("---------------")
+        io_handler.write("Inproceedings references")
+        io_handler.write("---------------\n ")
+        for entry in self.service.get_all_inproceedings():
+            io_handler.write("Cite Key     : " + entry[0])
+            io_handler.write("Author       : " + entry[1])
+            io_handler.write("Title        : " + entry[2])
+            io_handler.write("Book Title   : " + entry[3])
+            io_handler.write("Year         : " + str(entry[4]))
+            if entry[5] != "":
+                io_handler.write("Editor       : " + entry[5])
+            if entry[6] != "":
+                io_handler.write("Volume       : " + str(entry[6]))
+            if entry[7] != "":
+                io_handler.write("Number       : " + str(entry[7]))
+            if entry[8] != "":
+                io_handler.write("Series       : " + str(entry[8]))
+            if entry[9] != "":
+                io_handler.write("Pages        : " + entry[9])
+            if entry[10] != "":
+                io_handler.write("Address      : " + entry[10])
+            if entry[11] != "":
+                io_handler.write("Month        : " + str(entry[11]))
+            if entry[12] != "":
+                io_handler.write("Organization : " + entry[12])
+            if entry[13] != "":
+                io_handler.write("Publisher    : " + entry[13])
+            if entry[14] != "":
+                io_handler.write("Note         : " + entry[14])
+            io_handler.write("---------------")
+        sleep(2)
+        self.welcome(io_handler, service)
+
+    def export_bibtex_file(self, io_handler, service):
+        with open("file.bib", "w") as file:
+            all_book_refs=self.service.get_all_books()
+            for i in all_book_refs:
+                file.write("@book{" + i[0] + ",\n")
+                file.write("   author = {" + i[1] + "},\n")
+                file.write("   title = {" + i[2] + "},\n")
+                file.write("   year = {" + str(i[4]) + "},\n")
+                file.write("   publisher = {" + i[3] + "},\n")
+                if i[5] != "":
+                    file.write("   volume = {" + str(i[5]) + "},\n")
+                if i[6] != "":
+                    file.write("   number = {" + str(i[6]) + "},\n")
+                if i[7] != "":
+                    file.write("   pages = {" + i[7] + "},\n")
+                if i[8] != "":
+                    file.write("   month = {" + str(i[8]) +"- },\n")
+                if i[9] != "":
+                    file.write("   note = {" + i[9] + "},\n")
+                file.write("}\n\n")
+
+            all_article_refs=self.service.get_all_articles()
+            for j in all_article_refs:
+                file.write("@article{" + j[0] + ",\n")
+                file.write("   author = {" + j[1] + "},\n")
+                file.write("   title = {{" + j[2] + "}},\n") 
+                file.write("   year = {" + str(j[4]) + "},\n")
+                file.write("   journal = {" + j[3] + "},\n")
+                if j[5] != "":
+                    file.write("   volume = {" + str(j[5]) + "},\n")
+                if j[6] != "":
+                    file.write("   number = {" + str(j[6]) + "},\n")
+                if j[7] != "":
+                    file.write("   pages = {" + j[7] + "},\n")
+                if j[8] != "":
+                    file.write("   month = {" + str(j[8]) + "},\n")
+                if j[9] != "":
+                    file.write("   note = {" + j[9] + "},\n")
+                file.write("}\n\n")
+
+            all_inproceeding_refs=self.service.get_all_inproceedings()
+            for j in all_inproceeding_refs:
+                file.write("@inproceedings{" + j[0] + ",\n")
+                file.write("   author = {" + j[1] + "},\n")
+                file.write("   title = {{" + j[2] + "}},\n")
+                file.write("   booktitle = {" + j[3] + "},\n")
+                file.write("   year = {" + str(j[4]) + "},\n")
+                if j[5] != "":
+                    file.write("   editor = {" + j[5] + "},\n")
+                if j[6] != "":
+                    file.write("   volume = {" + str(j[6]) + "},\n")
+                if j[7] != "":
+                    file.write("   number = {" + str(j[7]) + "},\n")
+                if j[8] != "":
+                    file.write("   series = {" + str(j[8]) + "},\n")
+                if j[9] != "":
+                    file.write("   pages = {" + j[9] + "},\n")
+                if j[10] != "":
+                    file.write("   address = {" + j[10] + "},\n")
+                if j[11] != "":
+                    file.write("   month = {" + str(j[11]) +"- },\n")
+                if j[12] != "":
+                    file.write("   organization = {" + j[12] + "},\n")
+                if j[13] != "":
+                    file.write("   publisher = {" + j[13] + "},\n")
+                if j[14] != "":
+                    file.write("   note = {" + j[14] + "},\n")
+                file.write("}\n\n")
+        sleep(2)
+        self.welcome(io_handler, service)

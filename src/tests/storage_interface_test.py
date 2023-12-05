d7565573213c4a77d3e52eb0081d711cb23954c7
ds20220914 <davidkuul2002@gmail.com>
2023-12-05 03:07:22 +0200

    toka storage test done

--- src/tests/storage_interface_test.py
+++ src/tests/storage_interface_test.py
@@ -4,41 +4,58 @@ from storage_interface import StorageInterface  # Replace 'your_module' with the
 
 class TestStorageInterface(unittest.TestCase):
     def setUp(self):
-        # Create mock connections
         self.mock_connection1 = Mock()
         self.mock_connection2 = Mock()
         self.mock_connection3 = Mock()
 
-        # Create StorageInterface instance with mock connections
         self.storage_interface = StorageInterface(
             self.mock_connection1, self.mock_connection2, self.mock_connection3
         )
 
     def test_store_bookref(self):
-        # Create a mock book reference
         mock_bookref = Mock(
             key="mock_key", author=["Author"], title="Mock Title", publisher="Mock Publisher",
             year=2023, volume=1, number=1, pages="1-10", month="January", note="Mock Note"
         )
 
-        # Call the method to store the mock book reference
         self.storage_interface.store_bookref(mock_bookref)
 
-        # Assert that the execute method was called on the mock connection
         self.mock_connection1.cursor().execute.assert_called_once_with("INSERT INTO BReferences\
             (dbkey,author,title,publisher,year,volume,number,pages,month,note) \
             VALUES (?,?,?,?,?,?,?,?,?,?)",("mock_key", "Author",\
             "Mock Title", "Mock Publisher", 2023, 1,\
             1, "1-10", "January", "Mock Note"))
 
-        # Assert that commit was called on the mock connection
+
         self.mock_connection1.commit.assert_called_once()
 
-        # Query the database to check if the data was stored
+
         query = f"SELECT * FROM BReferences WHERE dbkey='mock_key'"
         result = self.mock_connection1.cursor().execute(query).fetchone()
 
-        # Assert that the result is not None, indicating that the data was stored
         self.assertIsNotNone(result)
 
+    def test_store_articleref(self):
+
+        mock_articleref = Mock(
+            key="mock_key1", author=["Author"], title="Mock Title", journal="MockPublisher1",
+            year=2023, volume=1, number=1, pages="1-10", month="January", note="Mock Note"
+        )
+
+        self.storage_interface.store_articleref(mock_articleref)
+
+        self.mock_connection2.cursor().execute.assert_called_once_with("INSERT INTO AReferences\
+            (dbkey,author,title,journal,year,volume,number,pages,month,note) \
+            VALUES (?,?,?,?,?,?,?,?,?,?)",("mock_key1", "Author",  \
+            "Mock Title", "MockPublisher1", 2023, 1,  \
+            1, "1-10", "January", "Mock Note"))
+
+
+        self.mock_connection2.commit.assert_called_once()
+
+
+        query = f"SELECT * FROM AReferences WHERE dbkey='mock_key1'"
+        result = self.mock_connection2.cursor().execute(query).fetchone()
+
+        self.assertIsNotNone(result)
 

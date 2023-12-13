81ea5321c5ffdf0c7d59ae86603867fc02cf6d2e
Ville Salmela <villesalmela@users.noreply.github.com>
2023-12-13 17:21:45 +0200

    delete unneeded files

--- src/db_build.py
+++
@@ -1,9 +0,0 @@
-from database_initialization import initialize_database
-
-
-def build():
-    initialize_database()
-
-if __name__ == "__main__":
-    # initialize database using build() command in command prompt
-    build()

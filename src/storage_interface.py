87cec5dd9fcc520aa1184a5d97745d1163433f03
Ville Salmela <villesalmela@users.noreply.github.com>
2023-12-13 17:35:15 +0200

    add demo data

--- src/storage_interface.py
+++ src/storage_interface.py
@@ -9,6 +9,75 @@ class StorageInterface:
         self._connection2=connection2
         self._connection3=connection3
 
+    def reset_test_data(self):
+        self.delete_all_references()
+        try:
+            rows = [
+                ("wilson_data", "Wilson", "The Art of Data", "NextGen Publishers", "2019", "4",
+                 "10", "297--303", "5", ""),
+                ("brown_demystified", "Brown", "Data Science Demystified", "TechBooks Publishing",
+                 "2020", "4", "4", "198--200", "2", ""),
+                ("emberson_practice", "Emberson", "Python in Practice", "NextGen Publishers",
+                 "2022", "4", "6", "568--569", "9", ""),
+                ("brown_automating", "Brown", "Automating with Python", "CodeHouse", "2020", "4",
+                 "9", "560", "1", ""),
+                ("daniels_solutions", "Daniels", "Reliable IT Solutions", "TechBooks Publishing",
+                 "2020", "1", "9", "497--501", "8", "")
+            ]
+            db_connection=self._connection1.cursor()
+            db_connection.executemany("INSERT INTO BReferences\
+            (dbkey,author,title,publisher,year,volume,number,pages,month,note) \
+            VALUES (?,?,?,?,?,?,?,?,?,?)",rows)
+
+            rows = [
+                ("miller_analysis", "Miller", "Data Analysis Techniques",
+                 "Science and Technology Review", "1990", "3", "7", "5--14", "6", ""),
+                ("brown_sustainable", "Brown", "Sustainable IT Solutions", "Journal of Computing",
+                 "2001", "3", "3", "70--73", "9", ""),
+                ("miller_ml", "Miller", "Advancements in Machine Learning",
+                 "Information Technology Today", "2001", "6", "16", "56--66", "1", ""),
+                ("jones_protocols", "Jones", "Network Security Protocols", "Future of IT", "1991",
+                 "10", "15", "74--90", "2", ""),
+                ("davis_ml", "Davis", "Threats in Machine Learning", "Tech Innovations", "1994",
+                 "18", "5", "20--30", "8", "")
+            ]
+            db_connection=self._connection2.cursor()
+            db_connection.executemany("INSERT INTO AReferences\
+            (dbkey,author,title,journal,year,volume,number,pages,month,note) \
+            VALUES (?,?,?,?,?,?,?,?,?,?)", rows)
+
+            rows = [
+                ("davis_blockchain", "Davis", "Blockchain Revolution",
+                "Proceedings of the 10th International Conference on Technology", "2017", "Anderson",
+                  "10", "1", "Emerging Trends in IT", "268--271", "Sydney", "4",
+                  "Foundation for IT Advancement", "Future Tech Books", ""),
+                ("merryb_quantum", "Merrybottom", "Advances in Quantum Computing",
+                 "International Workshop on Blockchain", "2011", "White", "7", "7",
+                 "Advances in Technology", "243--245", "Sydney", "8", "Global Tech Symposium",
+                 "Innovations Inc.", ""),
+                ("johnson_iot", "Johnson", "Innovations in IoT",
+                 "Proceedings of the 10th International Conference on Technology",
+                 "2001", "Anderson", "5", "10", "Innovations in Engineering", "68--69", "Tokyo",
+                 "10", "Network of Tech Academics", "Tech Press", ""),
+                ("garcia_today", "Garcia", "Cybersecurity Today", "Tech Trends in the 21st Century",
+                 "1993", "Moore", "8", "2", "Advances in Technology", "98--102", "Berlin", "9",
+                 "International Technology Association", "Cloud Computing Publications", ""),
+                ("roberts_beginners", "Roberts", "Deep Learning for Beginners",
+                 "International Workshop on Blockchain", "2011", "Jackson", "4", "8",
+                 "Critical Reviews in Computing", "196--207", "Sydney", "11",
+                 "Society for AI Innovations", "Cloud Computing Publications", "")
+            ]
+            db_connection=self._connection3.cursor()
+            db_connection.executemany("INSERT INTO IReferences\
+            (dbkey,author,title,booktitle,year,editor,volume,number,series,pages,\
+            address,month,organization,publisher,note)\
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", rows)
+        except Exception as exc:
+            raise exc
+        self._connection1.commit()
+        self._connection3.commit()
+        self._connection2.commit()
+
     def store_bookref(self, bookref):
         db_connection=self._connection1.cursor()
         author_field = self.stringify_list(bookref.author)

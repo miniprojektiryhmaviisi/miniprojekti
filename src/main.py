d42df5f7d98a4a8e2609bf4c00f82b8631c246ad
Ville Salmela <villesalmela@users.noreply.github.com>
2023-12-14 16:35:28 +0200

    change how demo mode is activated

--- src/main.py
+++ src/main.py
@@ -1,15 +1,24 @@
+import sys
+
 from references import References
 from console_io import ConsoleIO
 from repositories.referencesrepository import ReferencesRepository
 from services.reference_service import Services
 
-demo = True
+# launch normal-mode: 'python src/main.py'
+# launch demo-mode: 'python src/main.py demo'
+##   demo mode initializes database with test data
 
-def init():
+def init(demo: bool):
     console_io = ConsoleIO()
     ref_repo = ReferencesRepository()
     refe_services = Services(ref_repo, demo=demo)
     References(console_io, refe_services)
 
 if __name__ == "__main__":
-    init()
+    if len(sys.argv) >= 2:
+        mode = sys.argv[1]
+    else:
+        mode = "normal"   
+    demo = mode == "demo"
+    init(demo=demo)
